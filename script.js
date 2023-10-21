1a90242609a6427932b038bd70af087aa8cd245a
SAV-7-X <148570005+SAV-7-X@users.noreply.github.com>
2023-10-22 00:43:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+function loop() {
+
+
+    function C() {
+        document.getElementById('C').style.display = "inline";
+
+    }
+
+    function O() {
+        document.getElementById('O').style.display = "inline";
+
+    }
+
+    function D() {
+        document.getElementById('D').style.display = "inline";
+
+    }
+
+    function E() {
+        document.getElementById('E').style.display = "inline";
+
+    }
+
+    function R() {
+        document.getElementById('R').style.display = "inline";
+
+    }
+
+    function Rn() {
+        document.getElementById('R').style.display = "none";
+
+    }
+
+    function En() {
+        document.getElementById('E').style.display = "none";
+
+    }
+
+    function Dn() {
+        document.getElementById('D').style.display = "none";
+
+    }
+
+    function On() {
+        document.getElementById('O').style.display = "none";
+
+    }
+
+    function Cn() {
+        document.getElementById('C').style.display = "none";
+
+    }
+
+    function Gg() {
+        document.getElementById('Gg').style.display = "inline";
+
+    }
+
+    function Ag() {
+        document.getElementById('Ag').style.display = "inline";
+
+    }
+
+    function Mg() {
+        document.getElementById('Mg').style.display = "inline";
+
+    }
+
+    function Eg() {
+        document.getElementById('Eg').style.display = "inline";
+
+    }
+
+    function Rg() {
+        document.getElementById('Rg').style.display = "inline";
+
+    }
+
+    function Rgn() {
+        document.getElementById('Rg').style.display = "none";
+
+    }
+
+    function Egn() {
+        document.getElementById('Eg').style.display = "none";
+
+    }
+
+    function Mgn() {
+        document.getElementById('Mg').style.display = "none";
+
+    }
+
+    function Agn() {
+        document.getElementById('Ag').style.display = "none";
+
+    }
+
+    function Ggn() {
+        document.getElementById('Gg').style.display = "none";
+        loop();
+
+    }
+    setTimeout(C, 1000);
+    setTimeout(O, 1200);
+    setTimeout(D, 1400);
+    setTimeout(E, 1600);
+    setTimeout(R, 1800);
+    setTimeout(Rn, 3300);
+    setTimeout(En, 3500);
+    setTimeout(Dn, 3700);
+    setTimeout(On, 3900);
+    setTimeout(Cn, 4100);
+    setTimeout(Gg, 5300);
+    setTimeout(Ag, 5500);
+    setTimeout(Mg, 5700);
+    setTimeout(Eg, 5900);
+    setTimeout(Rg, 6100);
+    setTimeout(Rgn, 6600);
+    setTimeout(Egn, 6800);
+    setTimeout(Mgn, 7000);
+    setTimeout(Agn, 7200);
+    setTimeout(Ggn, 7400);
+    // setTimeout(loop, 7500);
+
+}
